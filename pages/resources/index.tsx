ab692c37ca299a7812038ba10ba3caedd71c35b3
Hamzawarraic <40853399+Hamzawarraic@users.noreply.github.com>
2022-08-16 00:54:19 +0500

    reources page ui style

--- pages/resources/index.tsx
+++ pages/resources/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner";
 import MainWrapper from "../style.js";
+import resourcesConsonents from "../../utils/resources.constants";
 
 const Resources = () => {
   return (
     <div>
       <Header />
       <MainWrapper>
+      <Banner
+          title={resourcesConsonents?.banner?.title}
+          bgImage={resourcesConsonents?.banner?.bgImage}
+          sideImage={resourcesConsonents?.banner?.sideImage}
+        />
       </MainWrapper>
       <Footer />
     </div>
