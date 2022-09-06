0bef2a315dd0b60f787410c649ce02a192f0f90f
Bilal Imrani <bimrani816@gmail.com>
2022-09-07 00:14:50 +0500

    changes

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -101,7 +101,7 @@ export default function Footer() {
         </div>
         <div className="address-wrapper1 row">
           <div className="col-12 py-2 py-lg-3 col-lg-10 text-center-responsivness">
-            <p>© {new Date().getFullYear()} BDATA Solution Inc.</p>
+            <p>© {new Date().getFullYear()} BDATA Solutions Inc.</p>
           </div>
           <div className="col-12 py-2 py-lg-3 col-lg-2 text-center-responsivness">
             <div className="socoalMediaLinks">
