54bfd85ff9898020403594a4df892c7ba7b40e30
Bilal Imrani <bimrani816@gmail.com>
2022-09-15 17:28:40 +0500

    changes

---
+++ pages/terms-conditions/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Head from "next/head";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+const TermsConditions = () => {
+  return (
+    <div>
+      <Head>
+        <title>Terms & Conditions</title>
+      </Head>
+      <Header />
+      Terms Conditions
+      <Footer />
+    </div>
+  );
+};
+
+export default TermsConditions;
