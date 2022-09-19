3418994de9ed85cdd8113ed01dde22ce7e042de6
Bilal Imrani <bimrani816@gmail.com>
2022-09-19 20:40:42 +0500

    changes

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 // import { AiOutlineLoading3Quarters } from "react-icons/ai";
-import Logo from "public/img/animatedLoader.gif";
+import Logo from "../../public/imganimatedLoader.gif";
 import LoaderWrapper from "./Loader.style";
 
 const LogoLoader = () => {
