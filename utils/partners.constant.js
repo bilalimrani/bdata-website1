5b12e31f0371a0b99b6e17fe45dae571a98cff81
Hamzawarraic <40853399+Hamzawarraic@users.noreply.github.com>
2022-09-28 22:37:49 +0500

    fix(ui):added enhancements

---
+++ utils/partners.constant.js
@@ -0,0 +1,7 @@
+export default {
+    banner: {
+      title: "BDATA Partner Space",
+      subTitle1: "At BDATA, we believe that partnerships grow together through the power of collaboration. Join the BDATA Partner Space and be part of a growing ecosystem, building the future of IoT.",
+  }
+};
+  
